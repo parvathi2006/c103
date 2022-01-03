91e8a107b56f288d4702fae314a8a48629434c5c
parvathi2006 <74611350+parvathi2006@users.noreply.github.com>
2022-01-04 01:01:08 +0530

    Add files via upload

--- c103.py
+++ c103.py
@@ -3,6 +3,5 @@ import plotly.express as px
 
 df = pd.read_csv("Copy+of+data+-+data.csv")
 fig = px.scatter(df, x = "date", y = "cases",
-                 color = "country", 
-                 size_max = 60)
+                 color = "country")
 fig.show()
